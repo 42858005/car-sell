6362c6d0184f1e1bd33b6dfd1cbe86b82e8f678b
Sww <15195036153@163.com>
2019-07-01 11:14:51 +0800

    111

---
+++ src/main/java/com/swj/carsell/controller/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.swj.carsell.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @PostMapping("/hello")
+    public String hello() {
+        return "hello";
+    }
+
+}
